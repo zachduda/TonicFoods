3c043211e18f14ebe0a04058add625b4f01cafe0
Zach Duda <zach@zachduda.com>
2024-04-24 22:11:00 -0400

    add bukkit chatcolor missing import

--- src/com/zachduda/tonicfoods/Utils.java
+++ src/com/zachduda/tonicfoods/Utils.java
@@ -9,6 +9,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -196,24 +197,24 @@ public class Utils {
     }
 
     static String convertColor(String msg) {
-		msg = msg.replaceAll("&#", "#");
-		Pattern pattern = Pattern.compile("(&#|#|&)[a-fA-F0-9]{6}");
-		Matcher matcher = pattern.matcher(msg);
-		while (matcher.find()) {
-			String hexCode = msg.substring(matcher.start(), matcher.end());
-			String replaceAmp = hexCode.replaceAll("&#", "x");
-			String replaceSharp = replaceAmp.replace('#', 'x');
+	msg = msg.replaceAll("&#", "#");
+	Pattern pattern = Pattern.compile("(&#|#|&)[a-fA-F0-9]{6}");
+	Matcher matcher = pattern.matcher(msg);
+	while (matcher.find()) {
+		String hexCode = msg.substring(matcher.start(), matcher.end());
+		String replaceAmp = hexCode.replaceAll("&#", "x");
+		String replaceSharp = replaceAmp.replace('#', 'x');
 
-			char[] ch = replaceSharp.toCharArray();
-			StringBuilder builder = new StringBuilder();
-			for (char c : ch) {
-				builder.append("&").append(c);
-			}
-
-			msg = msg.replace(hexCode, builder.toString());
-			matcher = pattern.matcher(msg);
+		char[] ch = replaceSharp.toCharArray();
+		StringBuilder builder = new StringBuilder();
+		for (char c : ch) {
+			builder.append("&").append(c);
 		}
-		return ChatColor.translateAlternateColorCodes('&', msg);
+
+		msg = msg.replace(hexCode, builder.toString());
+		matcher = pattern.matcher(msg);
+	}
+	return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
     private static List < String > convertListColor(List < String > msg) {
