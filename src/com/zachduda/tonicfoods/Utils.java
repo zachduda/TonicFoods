91716062fb3e8683a359ce966f72ebfa5721dfc9
Zach Duda <zach@zachduda.com>
2024-04-24 22:04:20 -0400

    update color conversion logic

--- src/com/zachduda/tonicfoods/Utils.java
+++ src/com/zachduda/tonicfoods/Utils.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import me.dave.chatcolorhandler.ChatColorHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -195,7 +194,24 @@ public class Utils {
     }
 
     static String convertColor(String msg) {
-        return ChatColorHandler.translateAlternateColorCodes(msg);
+		msg = msg.replaceAll("&#", "#");
+		Pattern pattern = Pattern.compile("(&#|#|&)[a-fA-F0-9]{6}");
+		Matcher matcher = pattern.matcher(msg);
+		while (matcher.find()) {
+			String hexCode = msg.substring(matcher.start(), matcher.end());
+			String replaceAmp = hexCode.replaceAll("&#", "x");
+			String replaceSharp = replaceAmp.replace('#', 'x');
+
+			char[] ch = replaceSharp.toCharArray();
+			StringBuilder builder = new StringBuilder();
+			for (char c : ch) {
+				builder.append("&").append(c);
+			}
+
+			msg = msg.replace(hexCode, builder.toString());
+			matcher = pattern.matcher(msg);
+		}
+		return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
     private static List < String > convertListColor(List < String > msg) {
